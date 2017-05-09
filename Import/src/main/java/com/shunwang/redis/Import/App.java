487f1ce7b08bdf3dbe182471fd470fc105debbf3
191644807 <191644807@qq.com>
2017-05-09 15:43:11 +0800

    aaaa

---
+++ Import/src/main/java/com/shunwang/redis/Import/App.java
@@ -0,0 +1,13 @@
+package com.shunwang.redis.Import;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
